1fb11cc4b7c52ed103af6ba3a445fe94466c07cf
BurnEmDown <amitreuveni@gmail.com>
2019-09-27 11:50:42 +0300

    1st commit

---
+++ Laser Defender/Assets/Scripts/Laser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] float projectileSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+    }
+
+}
